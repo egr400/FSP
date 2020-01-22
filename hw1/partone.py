511dab986213c4609e6c4d7ea65a2406554d6107
kosmos <42978035+ko-sm-os@users.noreply.github.com>
2020-01-21 18:44:47 -0800

    python hw1

---
+++ hw1/partone.py
@@ -0,0 +1,22 @@
+
+# =========== Problem 1 ==========
+print('in python 3 to print out stuff you have to include parentheses like this print(), but in python 2 you simply '
+      'just print and include stuff in there ')
+
+# =========== Problem 2 ==========
+x = [0, 1, 2, 3, 4, 5, 6]
+print(x[2])
+
+# =========== Problem 3 ==========
+y = x[:: -1]
+print(y)
+
+# =========== Problem 4 ==========
+z = x[1::2]
+print(z)
+
+# =========== Problem 5 ==========
+x = 99
+if (x > 0):
+    print('x is positive')
+
