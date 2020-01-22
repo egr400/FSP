26992b5170963ebc49f7e1cb8718e6a13fc98ac9
Saul-Maker <59861437+Saul-Maker@users.noreply.github.com>
2020-01-22 14:31:25 -0800

    Add files via upload

--- hw1/Homework1.py
+++ hw1/Homework1.py
@@ -1,109 +1,85 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 15 22:04:05 2020
-
-@author: Monica
-"""
-##Homework 1
-
-########PART ONE
-##1 
-difference = str('2 uses str type ASCII, while 3 uses Unicode')
-print("1.1 One difference between python 2 and 3 is that",
-      difference)
-
-##2
-#given list
+#Part 1
+#1.
+print("The print function is treated as a statement in python"
+      " 2 and it is treated as a method is python 3")
+#2.
 x = [0,1,2,3,4,5,6]
-#print the third item (n-1)
-print('2.1', x[2])
+print(x[2])
 
-##3
-#reverse the list
-reversed_list = x[::-1]
-#make y the reverse
-y= reversed_list
-#print y
-print('3.1', y)
-
-##4
-z= slice(1,6,2)
-print('4.1', x[z])
-
-##5
-x= 99
-if (x> 0) is True:
-    print('x is positive')
-    
-########PART TWO
-##1
-F = [0,1,1]
-for i in range(3,23):
-    F.append(F[i-1]+ F[i-2])
-    print('1.2', F)
-    
-##2
-x = [2.0, 3.0, 5.0, 7.0, 9.0]
+#3
 y = []
-for i in x:
-    y.append(i * 3 ** 2)
-    print('2.2', y)
+n = [-1]
 
-##3
-import math
+for n in x[::-1]:
+   y.append(n)
+
+print (y)
+
+#4
+z = x[1:2] + x[3:4] + x[5:6]
+
+print (z)
+
+#5
+x = 99
+if (x > 0):
+   print("X is Positive")
+#PartII
+
+#1
+n = 23 #set nth terms
+F = [0,0,1] # initial list
+N = range(n) #create iterative list
+i = 3 # set starting point
+for s in N:
+      F.append(F[i-1] + F[i-2])
+      i+=1
+
+print(F)
+
+#2
+x = [2.0 ,3.0,5.0,7.0,9.0]
+y =[]
+for s in x:
+   y.append( ( ( (3.0*s)**2)   / ((99*s) - (s**3))   )
+                - (1/s)  )
+
+print (y)
+
+#3
 a = 3.3
 b = 1.7
 c = -9.4
 
-mult_4 = 4 * a * c
-b_squared = b** 2
-root = math.sqrt(b_squared - mult_4)
-denominator = 2 * a
-plus = -b + root
-sub = -b - root
-x0 = plus / denominator
-x1 = sub / denominator
+x0 = ((-1*b) + ((b*b)-(4*a*c))**(1/2) ) / (2*a)
+x1 = ((-1*b) - ((b*b)-(4*a*c))**(1/2) ) / (2*a)
 
-print('3.2', x1, x0)
+print (x0 , x1)
 
-##4
-x = 1
-while x**2 <= 2000:
-    print('4.2',x)
-    x += 1
-     
-##5
-import math
-def volume():
-    r = .69
-    
-    radius_cubed = r **3
-    total_volume = 4 / 3 * math.pi * radius_cubed
-    print("the volume of a sphere with a radius of",
-          r, "is", total_volume)
-    
-def surface_area():
-    r = .4
-    
-    radius_squared = r ** 2
-    total_SA = 4 * math.pi * radius_squared
-    print("the surface area of a sphere with radius of",
-          r, "is", total_SA)
+#4
+squ = 0
+T = 2000
+p = 0
+while squ < T:
+     p += 1
+     squ = p*p
+     if squ > T or squ == T: p-=1
 
-def density():
-    #copy paste volume(), imput the new r 
-    r1 = .3
-    m1 = .25
-    r2 = .25
-    m2 = 2
-    radius_cubed1 = r1 **3
-    total_volume1 = 4 / 3 * math.pi * radius_cubed1
-    density_equation1 = m1 / total_volume1
-    radius_cubed2 = r2 **3
-    total_volume2 = 4 / 3 * math.pi * radius_cubed2
-    density_equation2 = m2 / total_volume2
-    print('density of the first:', density_equation1,
-          'density of second:', density_equation2)
-volume()   
-surface_area()
-density()
+print(p)
+
+#5
+m = 0.35
+
+def volume (r):
+      return ((4/3) * 3.14159 * (r**3))
+
+def sur_Area (r):
+      return (4 * 3.14159 * r * r)
+
+def density (m, v):
+      return (m/v)
+
+print(volume(0.69))
+print(sur_Area(0.4))
+print(density(m,volume(0.3)))
+print(density(2.0,volume(0.25)))
