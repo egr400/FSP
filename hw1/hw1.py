7bd72c6c09e71942d5d64e24d6be4eced4df6b03
JacobN17 <42988213+JacobN17@users.noreply.github.com>
2020-01-21 18:49:33 -0800

    Add files via upload

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,84 +1,146 @@
-
-# =========== Problem 1 ==========
-print('in python 3 to print out stuff you have to include parentheses like this print(), but in python 2 you simply '
-      'just print and include stuff in there ')
-
-# =========== Problem 2 ==========
-x = [0, 1, 2, 3, 4, 5, 6]
-print(x[2])
-
-# =========== Problem 3 ==========
-y = x[:: -1]
-print(y)
-
-# =========== Problem 4 ==========
-z = x[1::2]
-print(z)
-
-# =========== Problem 5 ==========
-x = 99
-if (x > 0):
-    print('x is positive')
-
-
-
-# ========== part II ===========
-
+# global variable list
 import math
+x = [0, 1, 2, 3, 4, 5, 6]
 
-# =========== Problem 1 ==========
-F = [0, 1, 1]
-for num in range(2, 22):
-    if (num < 2):
-        F.append(num)
-    else:
-        num = F[1] + F[2]
-        F[1] = F[2]
-        F[2] = num
-        print(num)
 
-# =========== Problem 2 ==========
-x = [2.0, 3.0, 5.0, 7.0, 9.0]
-Y = []
+# Part 1
+# 1
+def differences_in_python():
+    answer = "One difference between python2 and 3 is the print statement, within python2 there are multiple ways to " \
+             "construct statements. \n" "They can be done with parenthesis surrounding the string literal or no \n" \
+             "parenthesis at all, however in python3 this would cause an error."
 
-for i in x:
-    new_list = (3.0 * i) ** 2 / (99 * i - i ** 3) - 1 / i
-    Y.append(new_list)
+    print(answer, end="\n")
+
+
+differences_in_python()
+
+
+# 2
+def print_third_item():
+
+    print(x[3])
+
+
+print_third_item()
+
+
+# 3
+def reverse_x():
+
+    y = x[::-1]
+    print(y)
+
+
+reverse_x()
+
+
+# 4
+def list_slicing():
+    z = x[1::2]
+
+    print(z)
+
+
+list_slicing()
+
+
+# 5
+def code_correction():
+    x = 99
+    if (x > 0) is True:
+        print('x is positive')
+        print("No colon was inserted after if and does not follow Python syntax.")
+
+
+code_correction()
+
+
+# Part 2
+# 1
+def fib_sequence():
+
+    f = [0, 1, 1]
+    for i in range(20):
+        n = f[i+1] + f[i+2]
+        f.append(n)
+
+    print(f)
+
+
+fib_sequence()
+
+
+# 2
+def power_operation():
+    x = [2.0, 3.0, 5.0, 7.0, 9.0]
+    Y = []
+    for i in x:
+        n = (((3.0 * i)**2 / (99 * i - i ** 3)) - 1 / i)
+        Y.append(n)
     print(Y)
 
 
-# =========== Problem 3 ==========
-def quad(a, b, c):
-    xo = (-b + math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
-    x1 = (-b - math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
-    return xo, x1
+power_operation()
 
 
-print(quad(a=3.3, b=1.7, c=-9.4))
+# 3
+def quadratic_equation():
+
+    a = 3.3
+    b = 1.7
+    c = -9.4
+
+    x0 = ((-b + math.sqrt(b ** 2 - 4 * a * c)) / 2 * a)
+    x1 = ((-b - math.sqrt(b ** 2 - 4 * a * c)) / 2 * a)
+
+    print(x0)
+    print(x1)
 
 
-# =========== Problem 4 ==========
-largest = 0
-while (not largest ** 2 >= 2000):
-    largest += 1
-    print(largest-1)
+quadratic_equation()
+
+
+# 4
+def find_largest_int():
+
+    x = 1
+    while (x+1) ** 2 < 2000:
+        x += 1
+    print(x)
+
+
+find_largest_int()
+
+
+# 5
+def calculate_volume(r):
+
+    v = ((4 / 3) * math.pi * r ** 3)
+    return v
+
+
+print(calculate_volume(0.69))
+
+
+def calculate_surface_area(r):
+
+    A = (4 * math.pi * (r ** 2))
+    return A
+
+
+print(calculate_surface_area(0.4))
+
+
+def calculate_density(r, m):
+
+    p = (m / calculate_volume(r))
+    return p
+
+
+print(calculate_density(0.3, 0.35))
+print(calculate_density(0.25, 2.0))
 
 
 
-# =========== Problem 5 ==========
-def volume(v):
-    return 4 / 3 * math.pi * v ** 3
 
-
-def surface_area(A):
-    return 4 * math.pi * A ** 2
-
-
-def density(r, m=0.35):
-    return m / volume(r)
-
-
-print(volume(0.69))
-print(surface_area(0.4))
-print(density(0.3))
-print(density(0.25, m=2.0))
