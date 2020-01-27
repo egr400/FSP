caf7f057518b2b5802292b2b0a2977fd89b6d0ef
noahcschmidt <35358649+noahcschmidt@users.noreply.github.com>
2020-01-26 18:58:01 -0800

    Add files via upload

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,146 +1,88 @@
-# global variable list
+#Noah Schmidt, 642383
+#EGR400
+
 import math
-x = [0, 1, 2, 3, 4, 5, 6]
 
-
-# Part 1
-# 1
-def differences_in_python():
-    answer = "One difference between python2 and 3 is the print statement, within python2 there are multiple ways to " \
-             "construct statements. \n" "They can be done with parenthesis surrounding the string literal or no \n" \
-             "parenthesis at all, however in python3 this would cause an error."
-
-    print(answer, end="\n")
-
-
-differences_in_python()
-
-
-# 2
-def print_third_item():
-
-    print(x[3])
-
-
-print_third_item()
-
-
-# 3
-def reverse_x():
-
-    y = x[::-1]
-    print(y)
-
-
-reverse_x()
-
-
-# 4
-def list_slicing():
-    z = x[1::2]
-
-    print(z)
-
-
-list_slicing()
-
-
-# 5
-def code_correction():
-    x = 99
-    if (x > 0) is True:
-        print('x is positive')
-        print("No colon was inserted after if and does not follow Python syntax.")
-
-
-code_correction()
-
-
-# Part 2
-# 1
-def fib_sequence():
-
-    f = [0, 1, 1]
-    for i in range(20):
-        n = f[i+1] + f[i+2]
-        f.append(n)
-
-    print(f)
-
-
-fib_sequence()
-
-
-# 2
-def power_operation():
-    x = [2.0, 3.0, 5.0, 7.0, 9.0]
-    Y = []
-    for i in x:
-        n = (((3.0 * i)**2 / (99 * i - i ** 3)) - 1 / i)
-        Y.append(n)
-    print(Y)
-
-
-power_operation()
-
-
-# 3
-def quadratic_equation():
-
-    a = 3.3
-    b = 1.7
-    c = -9.4
-
-    x0 = ((-b + math.sqrt(b ** 2 - 4 * a * c)) / 2 * a)
-    x1 = ((-b - math.sqrt(b ** 2 - 4 * a * c)) / 2 * a)
-
-    print(x0)
-    print(x1)
-
-
-quadratic_equation()
-
-
-# 4
-def find_largest_int():
-
-    x = 1
-    while (x+1) ** 2 < 2000:
-        x += 1
-    print(x)
-
-
-find_largest_int()
-
-
-# 5
-def calculate_volume(r):
-
-    v = ((4 / 3) * math.pi * r ** 3)
-    return v
-
-
-print(calculate_volume(0.69))
-
-
-def calculate_surface_area(r):
-
-    A = (4 * math.pi * (r ** 2))
-    return A
-
-
-print(calculate_surface_area(0.4))
-
-
-def calculate_density(r, m):
-
-    p = (m / calculate_volume(r))
-    return p
-
-
-print(calculate_density(0.3, 0.35))
-print(calculate_density(0.25, 2.0))
-
-
-
-
+def main():
+	print("PART 1")
+	#1
+	q1string = "1) One difference between python 2 and 3 is that python 3 requires print\
+	statements to include parentheses."
+
+	#2
+	print(q1string)
+	x = [0, 1, 2, 3, 4, 5, 6]
+	print("2) The third item in x is " + str(x[3]))
+
+	#3
+	y = x[:]
+	y.reverse()
+	print("3) " + str(y))
+
+	#4
+	z = x[1::2]
+	print("4) " + str(z))
+
+	print("5) ")
+	#5
+	x = 99
+	if x > 0:
+		print('x is positive')
+
+	print()
+	print("PART 2")
+
+	#1
+	F = [0, 1, 1]
+	while len(F) < 23:
+		F.append(nextFib(F[-2:]))
+	print("1) " + str(F))
+
+	#2
+	x = [2.0, 3.0, 5.0, 7.0, 9.0]
+	y = []
+	for n in x:
+		y.append(nextVal(n))
+	print("2) " + str(y))
+
+	#3
+	x0, x1 = quadratic(3.3, 1.7, -9.4)
+	print("3) X0 is " + str(x0) + ", X1 is " + str(x1))
+
+	#4
+	x = 1
+	while x**2 < 2000:
+		x += 1
+	print("4) " + str(x-1))
+
+	#5
+	print("5) ")
+	print(surfArea(0.4))
+	print(density(0.3))
+	print(density(0.25, 2.0))
+
+
+
+def nextFib(inList):
+	return inList[0] + inList[1]
+
+def nextVal(n):
+	return (((3.0*n)**2) / ((99*n) - (n**3))) - (1 / n)
+
+def quadratic(a, b, c):
+	inner = ((b**2) - (4 * a * c))**(1/2)
+	x0 = (-b + inner) / (2 * a)
+	x1 = (-b - inner) / (2 * a)
+	return x0, x1
+
+def volume(rad):
+	return (4/3) * math.pi * (rad**3)
+
+def surfArea(rad):
+	return 4 * math.pi * (rad**2)
+
+def density(rad, mass=0.35):
+	return mass / volume(rad)
+
+if __name__ == "__main__":
+	main()
