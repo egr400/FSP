7f2da35525a837b089e00383dc9c56fb87759b5f
Saul-Maker <59861437+Saul-Maker@users.noreply.github.com>
2020-01-28 15:57:45 -0800

    Add files via upload

--- hw2/Homework_2.py
+++ hw2/Homework_2.py
@@ -1,49 +1,30 @@
 import math
-# Imports the math library to use the value of pi easily for volume and surface area later
+#Saul Morales & Malane Lieng
 
-def main():
+#---------Problem 1----------------------------------------------
+class sphere:
 
-pi = math.pi
 
 
-class Sphere:
-# Creates a new class name Sphere
 
-    def __init__(self, radius, mass): 
+    def __init__(self, radius, mass):
         self.radius = radius
         self.mass = mass
-# Defines the initial values for the class Sphere
+        self.v = (4/3) * math.pi * (self.radius**3)
+        self.a = (4) * math.pi * self.radius * self.radius
+        self.d = self.mass / self.v
 
-    def volume(self):
-        return float((4 / 3) * pi * self.radius * self.radius * self.radius)
-# Defines volume for objects of class Sphere
+    def info(self):
+        return "Volume: {}; Surface Area: {}; Density: {}".format(self.v,self.a,self.d)
+pass
 
-    def surfacearea(self):
-        return float(4 * pi * self.radius * self.radius)
-# Defines surfacearea for objects of class Sphere
+red = sphere(1.7, 0.25)
 
-    def density(self):
-        return float(red.mass / red.volume())
-# Defines density for objects of class Sphere
+print(red.info())
 
-red = Sphere(1.7, 0.25)
-# Creating the object red as class Sphere
-
-print(red.volume()) 
-# Prints the volume of red print(red.surfacearea())
-print(red.surfacearea())
-# Prints the surface of red
-print(red.density())
-# Prints the density of red
-print(dir(red))
-# Prints a list of attributes and methods for the object red 
-
-x = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]]
-# Assigning values to lists inside of a bigger list called X
+#-------------------2 -----------------------------------------
+x = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10,11], [12, 13, 14, 15]]
 
 for i in x:
-    print(*i, sep=' & ')
-# Prints the values in each list in X separated by & and creates a newline inbetween lists
+    print(*i, sep=" & ")
 
-if __name__ == '__main__':
-    main()
