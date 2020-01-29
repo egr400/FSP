096cb8805bfbeaa07ff1399beba56744c68833b7
sehyeon9 <43022222+sehyeon9@users.noreply.github.com>
2020-01-28 16:50:57 -0800

    Add files via upload

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,32 +1,26 @@
-#HW Question 1
 import math
 
-class sphere:
 
+class sphere:
     def __init__(self, radius, mass):
         self.radius = radius
         self.mass = mass
 
-    def volume(self):
-        return 4/3*math.pi*self.radius**3
+    def get_sphere_volume(self):
+        return (4 / 3) * math.pi * (self.radius ** 3)
 
-    def surface_area(self):
-        return 4*math.pi*self.radius**2
+    def get_sphere_surface_area(self):
+        return 4 * math.pi * (self.radius ** 2)
 
-    def density(self):
-        return self.mass/(4/3)*math.pi*self.radius**3
+    def get_density_of_sphere(self):
+        return self.mass / self.get_sphere_volume()
+
 
 red = sphere(1.7, 0.25)
+print(red.get_sphere_volume())
+print(red.get_sphere_surface_area())
+print(red.get_density_of_sphere())
 
-print(dir(red))
-print(red.volume())
-print(red.surface_area())
-print(red.density())
-
-#HW Question 2
-
-x = [[0, 1, 2, 3], [4, 5, 6, 7],
-    [8, 9, 10, 11], [12, 13, 14, 15]]
-
-for i in range(len(x)):
-    print(*x[i], sep = ' & ')
+x = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]]
+for i in range(0, len(x)):
+    print(*x[i], sep=' & ')
