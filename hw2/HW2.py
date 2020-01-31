7fdbc0fe92ccdd7c8be97e488a5f11c4737282d4
Greg241 <60156112+Greg241@users.noreply.github.com>
2020-01-31 10:37:09 -0800

    Add files via upload

--- hw2/HW2.py
+++ hw2/HW2.py
@@ -1,47 +1,29 @@
 import math
+#Gregorio Lopez
 
+# HomeWork 2
 class sphere:
 
-	# Class attribute, all instances created from this class will have 
-	# a shape of sphere 
-	shape = 'sphere'
-	
-	# intiializer/instance attributes
-	def __init__ (self, name, radius, mass):
-		self.name = name
-		self.radius = radius
-		self.mass = mass
-
-	# use an instance method to get other attributes like volume, surface area, and density
-
-	# volume of sphere = (4/3)*pi*r^3
-	def calculate_volume(self, name, radius):
-		volume = (4/3) * math.pi * (math.pow(radius, 3))
-		return volume
-
-	# surface area of a sphere = 4*pi*r^2	
-	def calculate_surface_area(self, name, radius):
-		surface_area = 4 * math.pi * (math.pow(radius, 2))
-		return surface_area
-
-	def calculate_density(self, name, volume, mass):
-		density = volume/mass
-		return density
 
 
-def main():
-	red = sphere('red', 17, 0.25)
-	# print(red)
-	# print(red.radius, red.mass, red.name)
 
-	volume = red.calculate_volume(red.name, red.radius)
-	surface_area = red.calculate_surface_area(red.name, red.radius)
-	density = red.calculate_density(red.name, volume, red.mass)
+    def __init__(self, radius, mass):
+        self.radius = radius
+        self.mass = mass
+        self.v = (4/3) * math.pi * (self.radius**3)
+        self.a = (4) * math.pi * self.radius * self.radius
+        self.d = self.mass / self.v
 
-	print('volume: {}'.format(volume))
-	print('surface_area: {}'.format(surface_area))
-	print('density: {}'.format(density))
-	print('alphabetical listing of attributes in the scope of red: {}'.format(dir(red)))
+    def info(self):
+        return "Volume: {}; Surface Area: {}; Density: {}".format(self.v,self.a,self.d)
+pass
 
-if __name__ == '__main__':
-	main()
+red = sphere(1.7, 0.25)
+
+print(red.info())
+
+#-------------------2 -----------------------------------------
+x = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10,11], [12, 13, 14, 15]]
+
+for i in x:
+    print(*i, sep=" & ")
