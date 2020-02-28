528ac9e19d025295d7379c82334751a223aabf42
Saul-Maker <59861437+Saul-Maker@users.noreply.github.com>
2020-02-28 12:46:07 -0800

    Delete config.py

--- hw3/config.py
+++
@@ -1,6 +0,0 @@
-### config.py ###
-
-##connection string to database and used for other secret passwords Utilized to connect to the books database
-# Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
-
-DATABASE_URI = 'postgres+psycopg2://postgres:hello@localhost:5432/books'
